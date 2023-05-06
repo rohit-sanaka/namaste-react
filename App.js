0a7d2c3b207f743b4d6953875b56ac8aafb9e419
Rohit Sanaka <srohitkumar147@gmail.com>
2023-05-06 15:45:42 +0530

    First code with jsx

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-const parent = React.createElement("div", { class: "parent" }, [
-  React.createElement("div", { class: "child" }, [
-    React.createElement("h1", { class: "heading" }, "This is child1 h1"),
-    React.createElement("h2", { class: "heading" }, "This is child1 h2"),
-  ]),
-  React.createElement("div", { class: "child" }, [
-    React.createElement("h1", { class: "heading" }, "This is child2 h1"),
-    React.createElement("h2", { class: "heading" }, "This is child2 h2"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//How to actuallu write the react code with out jsx
+
+// const parent = React.createElement("div", { class: "parent" }, [
+//   React.createElement("div", { class: "child" }, [
+//     React.createElement("h1", { class: "heading" }, "This is child1 h1"),
+//     React.createElement("h2", { class: "heading" }, "This is child1 h2"),
+//   ]),
+//   React.createElement("div", { class: "child" }, [
+//     React.createElement("h1", { class: "heading" }, "This is child2 h1"),
+//     React.createElement("h2", { class: "heading" }, "This is child2 h2"),
+//   ]),
+// ]);
+const parent = <h2>Rohit kumar sanaka</h2>;
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
