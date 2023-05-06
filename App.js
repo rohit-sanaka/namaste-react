744c6b1679fd248deb00bcc7ad53327bdc542cb4
Rohit Sanaka <srohitkumar147@gmail.com>
2023-05-06 19:41:15 +0530

    yarn package manager added

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 //How to actuallu write the react code with out jsx
 
 // const parent = React.createElement("div", { class: "parent" }, [
@@ -13,9 +10,30 @@ import ReactDOM from "react-dom/client";
 //     React.createElement("h2", { class: "heading" }, "This is child2 h2"),
 //   ]),
 // ]);
-const parent = <h2>Rohit kumar sanaka</h2>;
-console.log(parent);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//React element
+const title = <h1>Rohit Kumar Sanaka 🚀</h1>;
+
+//React functional component
+const MainComponent = () => {
+  return (
+    // <> </> is nothing but <React.Fragement></React.Fragement>
+    // We can wrap multiple elements with out using div.
+    <>
+      <header>{title}</header>
+      <main>
+        <h2>This is the main secion</h2>
+        <div>
+          <p>This a p tag inside div which is inside main</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+root.render(<MainComponent />);
